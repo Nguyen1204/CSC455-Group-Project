0f89e91b18a87478470f84f590c08628d0300105
NickDuong4 <111215423+NickDuong4@users.noreply.github.com>
2023-11-08 22:48:12 -0600

    Updated productManagement.cpp
    May need to add function that increments product 1, product 2, product 3, etc. like his outut depending on how he wants the file to be appended

--- productManagement.cpp
+++ productManagement.cpp
@@ -1,17 +1,20 @@
 //productManagement.cpp
 
 #include "header.h"
-#include <iostream>
-#include <fstream>
 #include <regex>
+#include <fstream>
+#include <iostream>
 
-bool isValidProductID(const std::string& productID, const std::vector<Product>& products) {
-    if (productID.length() != 8 || productID.substr(0, 4) != "Prod" || !std::regex_match(productID.substr(4), std::regex("^[0-9]{5}$")))
+// Function to check if a string represents a valid product ID
+bool isValidProductID(const std::string& productID, const std::vector<Product>& products)
+{
+    // Check if it starts with 'Prod' and is followed by a five-digit number
+    if (productID.length() != 9 || productID.substr(0, 4) != "Prod" || !std::regex_match(productID.substr(4), std::regex("^[0-9]{5}$")))
     {
         return false;
     }
 
-
+    // Check if it's unique
     for (const Product& product : products) 
     {
         if (product.productID == productID) 
@@ -23,6 +26,67 @@ bool isValidProductID(const std::string& productID, const std::vector<Product>& 
     return true;
 }
 
+// Function to add a new product
+void addProduct(std::vector<Product>& products)
+{
+    Product newProduct;
+    std::cout << "Enter product ID (e.g., 'Prod00001'): ";
+    std::cin >> newProduct.productID;
+
+    if (!isValidProductID(newProduct.productID, products))
+    {
+        std::cout << "Invalid product ID. It must start with 'Prod' followed by a five-digit number, and it must be unique." << std::endl;
+        return;
+    }
+
+    std::cout << "Enter product name: ";
+    std::cin >> newProduct.productName;
+
+    std::cout << "Enter product price: ";
+    std::cin >> newProduct.productPrice;
+
+    std::cout << "Enter the number of available items: ";
+    std::cin >> newProduct.availableItems;
+
+    if (newProduct.availableItems <= 0)
+    {
+        std::cout << "Invalid number of available items. It must be a positive number." << std::endl;
+        return;
+    }
+
+    products.push_back(newProduct);
+
+    std::cout << "Product added to inventory." << std::endl;
+}
+
+void removeProduct(std::vector<Product>& products, const std::string& productID) 
+{
+    for (auto it = products.begin(); it != products.end(); ++it) 
+    {
+        if (it->productID == productID)
+        {
+            products.erase(it);
+            std::cout << "Product removed from inventory." << std::endl;
+            return;
+        }
+    }
+
+    std::cout << "Product with ID " << productID << " not found." << std::endl;
+}
+
+void listProducts(const std::vector<Product>& products) 
+{
+    std::cout << "List of Products in Inventory:\n";
+    for (const Product& product : products) 
+    {
+        std::cout << "Product ID: " << product.productID << std::endl;
+        std::cout << "Product Name: " << product.productName << std::endl;
+        std::cout << "Product Price: $" << product.productPrice << std::endl;
+        std::cout << "Available Items: " << product.availableItems << std::endl;
+        std::cout << "--------------------------\n";
+    }
+}
+
 void saveProductData(const std::vector<Product>& products)
 {
     std::ofstream productsFile("products.txt");
@@ -30,17 +94,17 @@ void saveProductData(const std::vector<Product>& products)
     {
         for (const Product& product : products)
         {
-            productsFile << product.productID << ","
-                         << product.productName << ","
-                         << product.productPrice << ","
-                         << product.availableItems << "\n";
+            productsFile << product.productID << "\n"
+                         << product.productName << "\n"
+                         << product.productPrice << "\n"
+                         << product.availableItems << "\n\n";
         }
         productsFile.close();
         std::cout << "Product data saved to 'products.txt'." << std::endl;
-    }
+    } 
 
     else 
     {
-        std::cerr << "Could not open 'products.txt'" << std::endl;
+        std::cerr << "Failed to open 'products.txt'" << std::endl;
     }
 }
