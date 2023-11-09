72e85697cbad991250f2f9df7c4a1421a9d0b0e1
Phuc Nguyen <103988612+Nguyen1204@users.noreply.github.com>
2023-11-08 22:14:35 -0600

    Update menu.cpp

--- menu.cpp
+++ menu.cpp
@@ -1,53 +1,51 @@
-#include "header.h"
 #include <iostream>
-using namespace std;
-
+#include "header.h"
 void showMenu() {
-    cout << "Menu Options:" << endl;
-    cout << "1. Customer registration" << endl;
-    cout << "2. Customer Removal" << endl;
-    cout << "3. Product addition" << endl;
-    cout << "4. Product removal" << endl;
-    cout << "5. Shopping" << endl;
-    cout << "6. View customer using customer ID" << endl;
-    cout << "7. Redeem rewards" << endl;
-    cout << "8. Quit" << endl;
-    cout << "Enter your choice: ";
+    std::cout << "Menu Options:" << std::endl;
+    std::cout << "1. Customer registration" << std::endl;
+    std::cout << "2. Customer Removal" << std::endl;
+    std::cout << "3. Product addition" << std::endl;
+    std::cout << "4. Product removal" << std::endl;
+    std::cout << "5. Shopping" << std::endl;
+    std::cout << "6. View customer using customer ID" << std::endl;
+    std::cout << "7. Redeem rewards" << std::endl;
+    std::cout << "8. Quit" << std::endl;
+    std::cout << "Enter your choice: ";
 }
 
 int main() {
     int choice;
     do {
         showMenu();
-        cin >> choice;
+        std::cin >> choice;
 
         switch (choice) {
             case 1:
-                cout << "Customer registration selected." << endl;
+                std::cout << "Customer registration selected." << std::endl;
                 break;
             case 2:
-                cout << "Customer Removal selected." << endl;
+                std::cout << "Customer Removal selected." << std::endl;
                 break;
             case 3:
-                cout << "Product addition selected." << endl;
+                std::cout << "Product addition selected." << std::endl;
                 break;
             case 4:
-                cout << "Product removal selected." << endl;
+                std::cout << "Product removal selected." << std::endl;
                 break;
             case 5:
-                cout << "Shopping selected." << endl;
+                std::cout << "Shopping selected." << std::endl;
                 break;
             case 6:
-                cout << "View customer using customer ID selected." << endl;
+                std::cout << "View customer using customer ID selected." << std::endl;
                 break;
             case 7:
-                cout << "Redeem rewards selected." << endl;
+                std::cout << "Redeem rewards selected." << std::endl;
                 break;
             case 8:
-                cout << "Exiting..." << endl;
+                std::cout << "Exiting..." << std::endl;
                 break;
             default:
-                cout << "Invalid choice. Please try again." << endl;
+                std::cout << "Invalid choice. Please try again." << std::endl;
         }
 
     } while (choice != 8);
