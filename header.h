f5d636b2d1e2f0b9961a2da4fff2f9c3d26641ac
NickDuong4 <111215423+NickDuong4@users.noreply.github.com>
2023-11-08 21:49:12 -0600

    Update header.h

--- header.h
+++ header.h
@@ -3,5 +3,23 @@ Nicolas Duong
 Sean Gascon */
 
 //header.h
+#ifndef HEADER_H
+#define HEADER_H
+
 #include <iostream>
+#include <vector>
 using namespace std;
+
+struct Product
+{
+    std::string productID;
+    std::string productName;
+    double productPrice;
+    int availableItems;
+};
+
+bool isValidProductID(const std::string& productID, const std::vector<Product>& products);
+
+void saveProductData(const std::vector<Product>& products);
+
+#endif
