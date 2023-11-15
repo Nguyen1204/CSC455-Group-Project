3ca8972fb4ee0f17cee34594c2dcd492854fa8f2
Phuc Nguyen <103988612+Nguyen1204@users.noreply.github.com>
2023-11-15 17:36:17 -0600

    Update rewardManagement.cpp

--- rewardManagement.cpp
+++ rewardManagement.cpp
@@ -1,5 +1,28 @@
 //rewardManagement.cpp
 
 #include "header.h"
-#include <iostream>
+
+void RewardSystem::setPointsPerAmount(int points) {
+    pointsPerAmount = points;
+}
+
+void RewardSystem::addGift(int pointsRequired, const std::string& giftName) {
+    gifts.push_back(std::make_pair(pointsRequired, giftName));
+}
+
+void RewardSystem::displayGifts() const {
+    std::cout << "Available gifts:" << std::endl;
+    for (const auto& gift : gifts) {
+        std::cout << "Points required: " << gift.first << ", Gift: " << gift.second << std::endl;
+    }
+}
+
+std::string RewardSystem::redeemGift(int totalPoints) const {
+    for (const auto& gift : gifts) {
+        if (totalPoints >= gift.first) {
+            return gift.second;
+        }
+    }
+    return "No gift available for the given points.";
+}
 
