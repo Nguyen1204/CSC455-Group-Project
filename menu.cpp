8500f61a6903b1cab5d05c6d9a04d252aa0193e5
NickDuong4 <111215423+NickDuong4@users.noreply.github.com>
2023-11-14 22:32:14 -0600

    Update menu.cpp

--- menu.cpp
+++ menu.cpp
@@ -64,12 +64,51 @@ int main() {
         int choice;
         std::cin >> choice;
 
+#include "header.h"
+#include "productManagement.cpp"
+#include "customerService.cpp"
+#include <iostream>
+#include <vector>
+
+
+//Another menu would could use (This was what I was using for my program
+int main() {
+    std::vector<Product> products;
+    std::vector<Customer> customers;
+
+    while (true) {
+        std::cout << "Product Management Menu:\n";
+        std::cout << "1. Customer Registration\n";
+        std::cout << "2. Customer Removal\n";
+        std::cout << "3. Product Additon\n";
+        std::cout << "4. Product Removal\n";
+        std::cout << "5. Shopping\n";
+        std::cout << "6. View Customer Using Customer ID\n";
+        std::cout << "7. Redeem Rewards\n";
+        std::cout << "7. Quit\n";
+        std::cout << "Enter your choice (1-7): ";
+
+        int choice;
+        std::cin >> choice;
+
         switch (choice) {
             case 1:
-                addProduct(products);
+                registerCustomer(customers);
                 break;
 
-            case 2: {
+            case 2:
+                return 0;
+                break;
+
+            case 3: 
+            {
+                addProduct(products);
+                saveProductData(products);
+                break;
+            }
+
+            case 4:
+            {
                 std::string productID;
                 std::cout << "Enter product ID to remove: ";
                 std::cin >> productID;
@@ -77,19 +116,21 @@ int main() {
                 break;
             }
 
-            case 3:
-                listProducts(products);
-                break;
-
-            case 4:
-                saveProductData(products);
-                break;
-
             case 5:
+                return 0;
+                break;
+
+            case 6:
+                return 0;
+
+            case 7:
+                return 0;
+
+            case 8:
                 return 0;
 
             default:
-                std::cout << "Invalid choice. Please enter a valid option (1-4)." << std::endl;
+                std::cout << "Invalid choice. Please enter a valid option (1-7)." << std::endl;
         }
     }
 
